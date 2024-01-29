37aa7017ed38f8d405ea0704fbcab7019c7aef7c
Braian <braianortiz1603@gmail.com>
2024-01-29 10:24:34 -0500

    docs: docs folder are updated

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-CJAKLPFH.mjs";import"./chunk-FMFNIYM3.mjs";import"./chunk-7ZY3SBFR.mjs";import"./chunk-25MUHFLU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
